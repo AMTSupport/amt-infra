79c3032c8827e13395bac43d71b2f7b6bfb167e5
DaRacci <DaRacci@users.noreply.github.com>
2025-09-14 00:36:04 +0000

    Commit from GitHub Actions (Automated Updates)

--- hosts/hudu/application/docker-image.nix
+++ hosts/hudu/application/docker-image.nix
@@ -1,7 +1,7 @@
 {
   imageName = "hududocker/hudu";
-  imageDigest = "sha256:f036c818bf3db48d7981965eea8ccc330282df9e246af376c63dbff00d2770be";
-  hash = "sha256-C64vkY2/a9U/x3T/G20U5c0dwpflmO0sv6F2FXJ08/Y=";
+  imageDigest = "sha256:fca740b2bc0d7a06812e2ae1e6c7fbb7a5a42868d9e6ae261bc5caecd786a325";
+  hash = "sha256-CoiHhIQIQgAZIloiS7/yuVUW50CBfupBIcPwhIzX5dU=";
   finalImageName = "hududocker/hudu";
   finalImageTag = "latest";
 }
